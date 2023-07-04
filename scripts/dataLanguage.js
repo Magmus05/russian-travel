982e0f243dd81a3085e7eaa1970a3db045dcb963
Magomed Musakadiev <musakadiev-mag@yandex.ru>
2023-07-04 19:36:42 +0300

    добавил интерактивность

---
+++ scripts/dataLanguage.js
@@ -0,0 +1,132 @@
+export const dataLang = {
+  ruLang: {
+    leadTitle: "Путешествия по России",
+    leadSubtitle: "Настоящая страна не в выпусках новостей, а здесь",
+    leadCaption: "ваша полка — верхняя",
+    introTitle: "Чего мы там не видели?",
+    introText:
+      "По опросам ВЦИОМ, 95% россиян мечтают куда-нибудь поехать, но только 36% планируют провести отпуск в родной стране. Мол, чего мы тут, дома, не видели? На самом деле, Россия — это целая вселенная с ласковым морем юга, густыми лесами Саян и суровыми льдами плато Путорана. А ещё увидеть все эти красоты можно без миллионов на счету, загранпаспорта и многочасовых перелетов. Как, например, Вера Башмакова — смелая молодая мама, которая взяла в охапку троих детей, усадила их в свою «Ладу» и проехала 20 тысяч километров по родной стране. Мы выбрали и описали некоторые интересные места, достойные вашего отпуска.",
+    oneIntroList: "Часовых поясов ",
+    twoIntroList: "Объектов природного наследия ЮНЕСКО ",
+    threeIntroList: "Объектов культурного наследия ЮНЕСКО ",
+    fourIntroList: "Природных заповедников ",
+    fiveIntroList: "Аэропортов ",
+    onePlaceTitleKushir: "Куршская&nbsp;&nbsp; коса",
+    onePlaceParagraphsKushir: `<p class="place__paragraph">Здесь, посреди лесов и песчаных дюн, вы сможете увидеть два водных горизонта — спокойного Куршского залива с одной стороны и подёрнутого рябью волн Балтийского моря с другой. Уникальная природная зона на краю российского анклава.</p>
+		<p class="place__paragraph">На этом Калининградская область не заканчивается. Для путешественника и исследователя там же по соседству — самая западная точка России, Балтийская коса, — и немецкое наследие россыпи небольших приморских городов. Атмосфера здешних мест исключает суету, окуная в спокойствие природы и запах стального, прохладного моря.</p>`,
+    twoPlaceTitleKolski: "Кольский",
+    twoPlaceParagraphsKolski: `<p class="place__paragraph">Почти весь полуостров находится за Полярным кругом. Саамская тундра, от которой на юг — 
+		тайга, а на север — Ледовитый океан, прикидывающийся Баренцевым морем.</p>
+	<p class="place__paragraph">Возможно, вы смотрели Звягинцева и даже слышали историю арктического фестиваля в Териберке. Возможно, слово «Хибины» не осталось под снегом школьных воспоминаний об уроках географии. Возможно, вы не интересовались пронизывающей земную кору сверхглубокой скважиной, а от апатитов вас давно накрывает апатия. Но ваша мечта увидеть северное сияние начинает сбываться с билетом
+		в Мурманск.</p>`,
+    threePlaceTitleAltay: "Алтай",
+    threePlaceParagraphsAltay: `<p class="place__paragraph">Алтай — одно из красивейших мест в России. 
+		В первую очередь из-за гор: если ехать вдоль хребта, вы увидите склоны, усыпанные соснами, горные реки и озёра. А если вы откроете в автомобиле окна, сможете познакомиться с невидимым чудом здешних мест — горным воздухом.</p>
+	<p class="place__paragraph">Климат на Алтае умеренный, поэтому ехать сюда лучше всего летом. Так вы увидите всё разнообразие местной флоры и фауны. По лесам Алтая бродят лоси, над хребтами летают орлы, а на равнинах пасутся косули. И знаменитые манулы — тоже обитатели Алтайского края.</p>`,
+    fourPlaceTitleBaykal: "Зимний Байкал",
+    fourPlaceParagraphsBaykal: `<p class="place__paragraph">Всем известен Байкал как крупнейшее озеро 
+		в мире. Многие также знают, что это самый большой источник пресной воды и одно из красивейших мест в России.</p>
+	<p class="place__paragraph">Конечно, это всё так. Но Байкал ещё идеальное место для соревнований по скийорингу. Это такой вид спорта, когда лыжник привязывает себя 
+		к мотоциклу, и тандем старается развить как можно бóльшую скорость на льду. В марте 2019 года на фестивале «Байкальская миля» был поставлен мировой рекорд — 197.011 км/ч.</p>`,
+    fivePlaceTitleKareliya: "Карелия",
+    fivePlaceParagraphsKareliya: `<p class="place__paragraph">Сибирь заканчивается не на Урале, а в Карелии: образующая тайгу сибирская лиственница не растёт западнее Водлозера. Зато здесь она вымахивает на 30 метров — леса карельских национальных парков из-за непроходимых болот никогда не знали топора. Некоторым соснам уже больше чем полтысячелетия. Прикоснитесь к живому существу, видевшему солнце раньше, чем увидал его Иван Грозный. 
+		В девственном лесу на сотню километров не встретишь тропы. А на редких тропинках деревья 
+		в паре метров от земли помечены медвежьими когтями. Чтобы все знали, кто тут хозяин.</p>`,
+    coverTitle: "До Байкала «на собаках»",
+    coverSubtitle:
+      "По мотивам учебной темы о Транссибе — путешествие от столицы до Байкала на электричках.",
+    footerLinkMap: "Карты",
+    footerLinkWeather: "Погода",
+    footerLinkTimetable: "Расписание",
+    footerLinkCalendar: "Календарь",
+    footerLinkTravellings: "Путешествия",
+    footerCopyright: "&#169; 2023. Мусакадиев Магомед",
+  },
+	//---------------------------------------------//
+  engLang: {
+    leadTitle: "Travelling in Russia",
+    leadSubtitle: "The real country is not in the news releases, but here",
+    leadCaption: "your shelf - is the upper one",
+    introTitle: "What didn't we see there?",
+    introText:
+      "According to VTsIOM polls, 95% of Russians dream of going somewhere, but only 36% plan to spend a vacation in their native country. Like, what haven't we seen here at home? In fact, Russia is a whole universe with the gentle sea of the south, the dense forests of the Sayan Mountains and the harsh ice of the Putorana plateau. And you can also see all these beauties without millions on your account, a passport and many hours of flights. Like, for example, Vera Bashmakova — a brave young mother who took three children in her arms, sat them in her Lada and drove 20 thousand kilometers across her native country. We have selected and described some interesting places worthy of your vacation.",
+    oneIntroList: "Time zones ",
+    twoIntroList: "UNESCO Natural Heritage Sites ",
+    threeIntroList: "UNESCO Cultural Heritage Sites ",
+    fourIntroList: "Nature reserves ",
+    fiveIntroList: "Airports ",
+    onePlaceTitleKushir: `Kushirskaya &nbsp;&nbsp; kosa`,
+    onePlaceParagraphsKushir: `<p class="place__paragraph">Here, in the middle of forests and sand dunes, you can see two water horizons — the calm Curonian Lagoon on one side and the rippling waves of the Baltic Sea on the other. A unique natural area on the edge of the Russian enclave.</p>
+		<p class="place__paragraph">The Kaliningrad Region does not end there. For the traveler and researcher, there is the westernmost point of Russia, the Baltic Spit, in the same neighborhood, and the German heritage of placers of small seaside towns. The atmosphere of these places eliminates the hustle and bustle, plunging into the tranquility of nature and the smell of the steel, cool sea.</p>`,
+    twoPlaceTitleKolski: "Kolskiy",
+    twoPlaceParagraphsKolski: `<p class="place__paragraph">Almost the entire peninsula is located beyond the Arctic Circle. The Sami tundra, from which to the south —
+		taiga, and to the north — the Arctic Ocean, pretending to be the Barents Sea.</p>
+	<p class="place__paragraph">You may have watched Zvyagintsev and even heard the story of the Arctic festival in Teriberka. Perhaps the word "Khibiny" did not remain under the snow of school memories of geography lessons. Perhaps you were not interested in the ultra-deep well penetrating the earth's crust, and apathy has been covering you for a long time. But your dream of seeing the Northern lights begins to come true with a ticket
+	to Murmansk.</p>`,
+    threePlaceTitleAltay: "Altai",
+    threePlaceParagraphsAltay: `<p class="place__paragraph">Altai is one of the most beautiful places in Russia.
+		First of all, because of the mountains: if you drive along the ridge, you will see slopes strewn with pine trees, mountain rivers and lakes. And if you open the windows in the car, you can get acquainted with the invisible miracle of these places — mountain air.</p>
+	<p class="place__paragraph">The climate in Altai is temperate, so it's best to go here in summer. So you will see all the diversity of the local flora and fauna. Moose roam the forests of Altai, eagles fly over the ridges, and roe deer graze on the plains. And the famous manuls are also inhabitants of the Altai Territory.</p>`,
+    fourPlaceTitleBaykal: "Winter Baikal",
+    fourPlaceParagraphsBaykal: `<p class="place__paragraph">Everyone knows Baikal as the largest lake
+		in the world. Many also know that this is the largest source of fresh water and one of the most beautiful places in Russia.</p>
+	<p class="place__paragraph">Of course, this is all true. But Baikal is still an ideal place for skioring competitions. This is a kind of sport when a skier ties himself
+	to the motorcycle, and the tandem tries to develop as much speed as possible on the ice. In March 2019, a world record of 197.011 km/h was set at the Baikal Mile Festival.</p>`,
+    fivePlaceTitleKareliya: "Karelia",
+    fivePlaceParagraphsKareliya: `<p class="place__paragraph">Siberia does not end in the Urals, but in Karelia: Siberian larch, which forms the taiga, does not grow west of the Water Lake. But here it reaches 30 meters — the forests of Karelian national parks have never known an axe because of impassable swamps. Some pines are already more than half a millennium old. Touch a living being who saw the sun before Ivan the Terrible saw it.
+		In the virgin forest, you will not find a trail for a hundred kilometers. And on rare paths, trees
+		a couple of meters from the ground are marked with bear claws. So that everyone knows who is the boss here.</p>`,
+    coverTitle: "To Baikal <br> «by dogs»",
+    coverSubtitle:
+      "Based on the educational theme about the Trans—Siberian Railway - a journey from the capital to Lake Baikal by train.",
+    footerLinkMap: "Map",
+    footerLinkWeather: "Weather",
+    footerLinkTimetable: "Timetable",
+    footerLinkCalendar: "Calendar",
+    footerLinkTravellings: "Travellings",
+    footerCopyright: "&#169; 2023. Musakadiev Magomed",
+  },
+  deLang: {
+    leadTitle: "Reisen in Russland",
+    leadSubtitle: "Das wahre Land ist nicht in den Nachrichten, sondern hier",
+    leadCaption: "ihr Regal ist oben",
+    introTitle: "Was haben wir dort nicht gesehen?",
+    introText:
+      "Laut Umfragen träumen 95% der Russen davon, irgendwohin zu gehen, aber nur 36% planen, einen Urlaub in ihrem Heimatland zu verbringen. Sie sagen, was haben wir hier zu Hause nicht gesehen? Tatsächlich ist Russland ein ganzes Universum mit dem sanften Meer des Südens, den dichten Wäldern der Saiyan und dem harten Eis des Putoran-Plateaus. Und Sie können all diese Schönheiten auch ohne Millionen auf dem Konto, einen Reisepass und stundenlange Flüge sehen. Wie zum Beispiel Vera Schuh ist eine mutige junge Mutter, die drei Kinder in den Arm nahm, sie in ihren «Bund» setzte und 20.000 Kilometer durch ihr Heimatland reiste. Wir haben einige interessante Orte ausgewählt und beschrieben, die Ihren Urlaub wert sind.",
+    oneIntroList: "Zeitzone ",
+    twoIntroList: "UNESCO-Naturerbe ",
+    threeIntroList: "UNESCO-Kulturerbe ",
+    fourIntroList: "Naturschutzgebiete ",
+    fiveIntroList: "Flughäfen ",
+    onePlaceTitleKushir: `Kurische &nbsp;&nbsp; Nehrung`,
+    onePlaceParagraphsKushir: `<p class="place__paragraph">Inmitten von Wäldern und Sanddünen können Sie hier zwei Wasserhorizonte sehen: den ruhigen Kurischen Golf auf der einen Seite und die von Wellen geprägte Ostsee auf der anderen Seite. Eine einzigartige Naturzone am Rande der russischen Enklave.</p>
+		<p class="place__paragraph">Damit endet das Kaliningrader Gebiet nicht. Für den Reisenden und Entdecker ist dort nebenan der westlichste Punkt Russlands, die baltische Nehrung — und das deutsche Erbe der kleinen Küstenstädte. Die Atmosphäre der hiesigen Orte schließt Hektik aus, taucht in die Ruhe der Natur ein und riecht nach kaltem, kaltem Meer aus Stahl.</p>`,
+    twoPlaceTitleKolski: "Kola",
+    twoPlaceParagraphsKolski: `<p class="place__paragraph">Fast die gesamte Halbinsel liegt hinter dem Polarkreis. Die samische Tundra, von der aus nach Süden —
+    die Taiga und der Arktische Ozean im Norden, der sich als Barentssee anfühlt.</p>
+	<p class="place__paragraph">Vielleicht haben Sie Zvyagintsev gesehen und sogar die Geschichte des arktischen Festivals in Teriberk gehört. Vielleicht ist das Wort «Hibiny» nicht unter dem Schnee der Schulerinnerungen an den Geographieunterricht geblieben. Vielleicht haben Sie sich nicht für eine extrem tiefe Bohrung interessiert, die die Erdkruste durchdringt, und Sie sind seit langem mit Apathie von Apathie bedeckt. Aber Ihr Traum, das Nordlicht zu sehen, wird mit einem Ticket wahr
+  nach Murmansk.</p>`,
+  threePlaceTitleAltay: "Altai",
+  threePlaceParagraphsAltay: `<p class="place__paragraph">Altai ist einer der schönsten Orte in Russland.
+  Vor allem wegen der Berge: Wenn Sie entlang des Gramms fahren, sehen Sie die mit Pinien übersäten Hänge, Gebirgsflüsse und Seen. Und wenn Sie die Fenster im Auto öffnen, können Sie das unsichtbare Wunder der hiesigen Orte, die Bergluft, kennenlernen.</p>
+<p class="place__paragraph">Das Klima im Altai ist gemäßigt, daher ist es am besten, im Sommer hierher zu fahren. So werden Sie die ganze Vielfalt der lokalen Flora und Fauna sehen. Elche wandern durch die Wälder des Altai, Adler fliegen über die Grate und Rehe grasen in den Ebenen. Und die berühmten Manul sind auch Bewohner des Altai-Territoriums.</p>`,
+  fourPlaceTitleBaykal: "Baikal im Winter",
+  fourPlaceParagraphsBaykal: `<p class="place__paragraph">Jeder kennt den Baikalsee als den größten See
+  in der Welt. Viele wissen auch, dass dies die größte Quelle für frisches Wasser und einer der schönsten Orte in Russland ist.</p>
+<p class="place__paragraph">Natürlich ist das alles so. Aber der Baikalsee ist immer noch der ideale Ort für Skijoringwettbewerbe. Es ist so ein Sport, wenn ein Skifahrer sich selbst anschnallt
+zum Motorrad, und das Tandem versucht, so viel Geschwindigkeit wie möglich auf dem Eis zu entwickeln. Im März 2019 wurde beim Festival «Baikalmeile» ein Weltrekord von 197.011 km / h aufgestellt.</p>`,
+  fivePlaceTitleKareliya: "Karelien",
+  fivePlaceParagraphsKareliya: `<p class="place__paragraph">Sibirien endet nicht im Ural, sondern in Karelien: Die sibirische Lärche, die die Taiga bildet, wächst nicht westlich des Wasserlosers. Aber hier weht sie 30 Meter hoch — die Wälder der karelischen Nationalparks kannten wegen der undurchdringlichen Sümpfe nie eine Axt. Einige Kiefern sind schon mehr als ein halbes Jahrtausend alt. Berühre ein Lebewesen, das die Sonne gesehen hat, bevor Iwan der Schreckliche sie gesehen hat.
+  In einem unberührten Wald für Hunderte von Kilometern wirst du keinen Weg finden. Und auf seltenen Wegen Bäume
+  ein paar Meter vom Boden entfernt sind sie mit Bärenklauen beschriftet. Damit jeder weiß, wer hier der Besitzer ist.</p>`,
+  coverTitle: "Bis zum Baikalsee «bei Hunden»",
+  coverSubtitle:
+    "Basierend auf dem Lehrthema über die Transsib — Reise von der Hauptstadt zum Baikalsee mit S-Bahnen.",
+  footerLinkMap: "Karten",
+  footerLinkWeather: "Das Wetter",
+  footerLinkTimetable: "Zeitplan",
+  footerLinkCalendar: "Der Kalender",
+  footerLinkTravellings: "Reisen",
+  footerCopyright: "&#169; 2023. Musakadiev Magomed",
+  },
+};
---
+++ scripts/script.js
@@ -0,0 +1,190 @@
+import { dataLang } from "./dataLanguage.js";
+const page = document.body.childNodes;
+const popupImage = page[3];
+const fotoGrid = page[1].children[1].children[2];
+const russianLang = page[1].children[0].children[1].children[0];
+const englishLang = page[1].children[0].children[1].children[1];
+const deutscheLang = page[1].children[0].children[1].children[2];
+
+
+//---------блок LEAD--------------\\
+//const leadTitle = page[1].children[1].children[0].children[0];
+//const leadSubtitle = page[1].children[1].children[0].children[1];
+//const leadCaption = page[1].children[1].children[0].children[3];
+
+//---------блок INTRO--------------\\
+//const introTitle = page[1].children[1].children[1].children[0];
+//const introText = page[1].children[1].children[1].children[1];
+//const introList = page[1].children[1].children[1].children[2].children
+
+//---------блок PLACE--------------\\
+//const onePlaceTitleKushir = page[1].children[1].children[3].children[0].children[0];
+//const onePlaceParagraphsKushir = page[1].children[1].children[3].children[0].children[3];
+//const twoPlaceTitleKolski = page[1].children[1].children[3].children[1].children[0];
+//const twoPlaceParagraphsKolski = page[1].children[1].children[3].children[1].children[3];
+//const threePlaceTitleAltay = page[1].children[1].children[3].children[2].children[0];
+//const threePlaceParagraphsAltay = page[1].children[1].children[3].children[2].children[3];
+//const fourPlaceTitleBaykal = page[1].children[1].children[3].children[3].children[0];
+//const fourPlaceParagraphsBaykal = page[1].children[1].children[3].children[3].children[3];
+//const fivePlaceTitleKareliya = page[1].children[1].children[3].children[4].children[0];
+//const fivePlaceParagraphsKareliya = page[1].children[1].children[3].children[4].children[3];
+
+//---------блок COVER--------------\\
+//const coverTitle = page[1].children[1].children[4].children[0].children[0];
+//const coverSubtitle = page[1].children[1].children[4].children[0].children[1];
+
+//---------блок Footer--------------\\
+// const footerLinkMap = page[1].children[2].children[0].children[0]
+// const footerLinkWeather = page[1].children[2].children[0].children[1]
+// const footerLinkTimetable = page[1].children[2].children[0].children[2]
+// const footerLinkCalendar = page[1].children[2].children[0].children[3]
+// const footerLinkTravellings = page[1].children[2].children[0].children[4]
+// const footerCopyright = page[1].children[2].children[1]
+
+function changeLanguage(selectedLang) {
+  //---------блок LEAD--------------\\
+  page[1].children[1].children[0].children[0].textContent = selectedLang.leadTitle;
+  page[1].children[1].children[0].children[1].textContent = selectedLang.leadSubtitle;
+  page[1].children[1].children[0].children[3].textContent = selectedLang.leadCaption;
+  //---------блок INTRO--------------\\
+  page[1].children[1].children[1].children[0].textContent = selectedLang.introTitle;
+  page[1].children[1].children[1].children[1].textContent = selectedLang.introText;
+  page[1].children[1].children[1].children[2].children[0].childNodes[0].textContent =
+    selectedLang.oneIntroList;
+  page[1].children[1].children[1].children[2].children[1].childNodes[0].textContent =
+    selectedLang.twoIntroList;
+  page[1].children[1].children[1].children[2].children[2].childNodes[0].textContent =
+    selectedLang.threeIntroList;
+  page[1].children[1].children[1].children[2].children[3].childNodes[0].textContent =
+    selectedLang.fourIntroList;
+  page[1].children[1].children[1].children[2].children[4].childNodes[0].textContent =
+    selectedLang.fiveIntroList;
+  //---------блок PLACE--------------\\
+  page[1].children[1].children[3].children[0].children[0].textContent = "";
+  page[1].children[1].children[3].children[0].children[0].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.onePlaceTitleKushir
+  );
+  page[1].children[1].children[3].children[0].children[3].textContent = "";
+  page[1].children[1].children[3].children[0].children[3].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.onePlaceParagraphsKushir
+  );
+
+  page[1].children[1].children[3].children[1].children[0].textContent =
+    selectedLang.twoPlaceTitleKolski;
+  page[1].children[1].children[3].children[1].children[3].textContent = "";
+  page[1].children[1].children[3].children[1].children[3].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.twoPlaceParagraphsKolski
+  );
+
+  page[1].children[1].children[3].children[2].children[0].textContent =
+    selectedLang.threePlaceTitleAltay;
+  page[1].children[1].children[3].children[2].children[3].textContent = "";
+  page[1].children[1].children[3].children[2].children[3].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.threePlaceParagraphsAltay
+  );
+  page[1].children[1].children[3].children[3].children[0].textContent =
+    selectedLang.fourPlaceTitleBaykal;
+  page[1].children[1].children[3].children[3].children[3].textContent = "";
+  page[1].children[1].children[3].children[3].children[3].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.fourPlaceParagraphsBaykal
+  );
+  page[1].children[1].children[3].children[4].children[0].textContent =
+    selectedLang.fivePlaceTitleKareliya;
+  page[1].children[1].children[3].children[4].children[3].textContent = "";
+  page[1].children[1].children[3].children[4].children[3].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.fivePlaceParagraphsKareliya
+  );
+  //---------блок COVER--------------\\
+  page[1].children[1].children[4].children[0].children[0].textContent ='';
+  page[1].children[1].children[4].children[0].children[0].insertAdjacentHTML(
+    "afterbegin", selectedLang.coverTitle);
+  page[1].children[1].children[4].children[0].children[1].textContent =
+    selectedLang.coverSubtitle;
+  //---------блок Footer--------------\\
+  page[1].children[2].children[0].children[0].textContent = selectedLang.footerLinkMap;
+
+  page[1].children[2].children[0].children[1].textContent =
+    selectedLang.footerLinkWeather;
+  page[1].children[2].children[0].children[2].textContent =
+    selectedLang.footerLinkTimetable;
+  page[1].children[2].children[0].children[3].textContent =
+    selectedLang.footerLinkCalendar;
+  page[1].children[2].children[0].children[4].textContent =
+    selectedLang.footerLinkTravellings;
+  page[1].children[2].children[1].textContent = "";
+  page[1].children[2].children[1].insertAdjacentHTML(
+    "afterbegin",
+    selectedLang.footerCopyright
+  );
+}
+
+russianLang.addEventListener("click", (e) => {
+  if (russianLang.classList.contains("header__lang-link_enabled")) {
+    return;
+  } else {
+    russianLang.classList.add("header__lang-link_enabled");
+    englishLang.classList.remove("header__lang-link_enabled");
+    deutscheLang.classList.remove("header__lang-link_enabled");
+    changeLanguage(dataLang.ruLang);
+  }
+});
+
+englishLang.addEventListener("click", (e) => {
+  if (englishLang.classList.contains("header__lang-link_enabled")) {
+    return;
+  } else {
+    englishLang.classList.add("header__lang-link_enabled");
+    russianLang.classList.remove("header__lang-link_enabled");
+    deutscheLang.classList.remove("header__lang-link_enabled");
+    changeLanguage(dataLang.engLang);
+  }
+});
+deutscheLang.addEventListener("click", (e) => {
+  if (deutscheLang.classList.contains("header__lang-link_enabled")) {
+    return;
+  } else {
+    deutscheLang.classList.add("header__lang-link_enabled");
+    russianLang.classList.remove("header__lang-link_enabled");
+    englishLang.classList.remove("header__lang-link_enabled");
+    changeLanguage(dataLang.deLang);
+  }
+});
+
+
+fotoGrid.addEventListener('click', (e)=>{
+	openPopup(popupImage)
+	popupImage.children[0].children[1].setAttribute('src', e.target.attributes[0].nodeValue);
+	popupImage.children[0].children[1].setAttribute('alt', `Фотография: ${e.target.alt}.`);
+	popupImage.children[0].children[2].textContent = e.target.alt;
+})
+
+function openPopup(popup){
+	popup.classList.add('popup_opened');
+	document.addEventListener('keydown', closeByEscape); 
+}
+function closePopup(popup){
+	popup.classList.remove('popup_opened');
+	document.removeEventListener('keydown', closeByEscape); 
+}
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
+}
+
+popupImage.addEventListener('mousedown', (evt)=>{
+	if(evt.target.classList.contains('popup_opened')){
+		closePopup(popupImage)
+	}
+	if (evt.target.classList.contains('popup-close-button')) {
+		closePopup(popupImage)
+	}
+})
+
